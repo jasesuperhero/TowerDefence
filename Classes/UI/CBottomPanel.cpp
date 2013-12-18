a16818682f860d768b171808a4cee5c7ea2d02b2
jase_superhero <jase.siperhero@gmail.com>
2013-12-19 00:26:01 +0400

    github commit

---
+++ Classes/UI/CBottomPanel.cpp
@@ -0,0 +1,9 @@
+//
+//  CBottomPanel.cpp
+//  HelloCpp
+//
+//  Created by Daniel on 02.12.13.
+//
+//
+
+#include "CBottomPanel.h"
---
+++ Classes/UI/CBottomPanel.h
@@ -0,0 +1,14 @@
+//
+//  CBottomPanel.h
+//  HelloCpp
+//
+//  Created by Daniel on 02.12.13.
+//
+//
+
+#ifndef __HelloCpp__CBottomPanel__
+#define __HelloCpp__CBottomPanel__
+
+#include <iostream>
+
+#endif /* defined(__HelloCpp__CBottomPanel__) */
